3eaf10bc3479d37c4a83dca65f18b7a065624a1d
JD037 <joj237@gmail.com>
2023-04-14 13:04:39 -0700

    last attempt before scrubbing

--- simple_shell.c
+++ simple_shell.c
@@ -1,26 +1,26 @@
-#include "shell.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
+#include <unistd.h>
 #include <errno.h>
+#include <sys/wait.h>
 
-#define PROMPT "#cisfun$ "
-#define BUFSIZE 1024
+#define MAX_COMMAND_LENGTH 100
+#define PROMPT "$ "
 
 int main(void)
 {
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t read;
-    pid_t child_pid;
+    char command[MAX_COMMAND_LENGTH];
     int status;
 
     while (1)
     {
+        pid_t pid;
+
         printf(PROMPT);
         fflush(stdout);
-        if ((read = getline(&line, &len, stdin)) == -1)
+
+        if (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL)
         {
             if (feof(stdin))
             {
@@ -29,59 +29,43 @@ int main(void)
             }
             else
             {
-                perror("getline");
+                perror("fgets");
                 exit(EXIT_FAILURE);
             }
         }
-        line[strcspn(line, "\n")] = '\0'; /* Remove trailing newline */
-        if ((child_pid = fork()) == -1)
+
+        command[strcspn(command, "\n")] = '\0';
+
+        pid = fork();
+
+        if (pid == -1)
         {
             perror("fork");
             exit(EXIT_FAILURE);
         }
-        if (child_pid == 0)
+        else if (pid == 0)
         {
-            char *argv[BUFSIZE];
-            int argc = 0;
-            char *token = strtok(line, " ");
-            while (token != NULL && argc < BUFSIZE - 1)
-            {
-                argv[argc++] = token;
-                token = strtok(NULL, " ");
-            }
-            argv[argc] = NULL;
-            if (execve(argv[0], argv, NULL) == -1)
+            char *argv[2] = { NULL };
+            argv[0] = command;
+
+            if (execvp(argv[0], argv) == -1)
             {
                 if (errno == ENOENT)
                 {
-                    printf("%s: command not found\n", argv[0]);
-                    exit(EXIT_FAILURE);
+                    printf("%s: command not found\n", command);
                 }
                 else
                 {
-                    perror("execve");
-                    exit(EXIT_FAILURE);
+                    perror("execvp");
                 }
+                exit(EXIT_FAILURE);
             }
         }
         else
         {
-            waitpid(child_pid, &status, 0);
-            if (WIFEXITED(status))
-            {
-                int exit_status = WEXITSTATUS(status);
-                if (exit_status != 0)
-                {
-                    printf("Command exited with status %d\n", exit_status);
-                }
-            }
-            else if (WIFSIGNALED(status))
-            {
-                int signal_number = WTERMSIG(status);
-                printf("Command terminated by signal %d\n", signal_number);
-            }
+            waitpid(pid, &status, 0);
         }
     }
-    free(line);
-    exit(EXIT_SUCCESS);
+
+    return 0;
 }
